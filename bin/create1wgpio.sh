42bab0e1d6a968821ed96bc90990ea1b12a44e58
Michael Schlenstedt <Michael@schlenn.net>
2022-11-12 10:24:12 +0100

    Bugfixes

--- bin/create1wgpio.sh
+++ bin/create1wgpio.sh
@@ -2,16 +2,20 @@
 
 pluginname=REPLACELBPPLUGINDIR
 
-gpio=$(jq '.gpio' $LBPCONFIG/$pluginname/owfs.json)
+gpio=$(jq -r '.gpio' $LBPCONFIG/$pluginname/owfs.json)
 
 if [ $gpio = "true" ]; then
 	grep -i "dtoverlay=w1-gpio,gpiopin=4" /boot/config.txt #überprüfen ob die enträge schon vorhanden sind
 	if [ $? -eq 0 ]; then 
 		echo"" #wenn sie schon vorhanden sind nichts tun
 	else 
-		echo "" >> /boot/config.txt # Leerzeile
-		pullup=$(jq '.usb' $LBPCONFIG/$pluginname/owfs.json)
+		#echo "" >> /boot/config.txt # Leerzeile
+		pullup=$(jq -r '.usb' $LBPCONFIG/$pluginname/owfs.json)
 		if [ $pullup = "true" ]; then 
+			lastline=$(tail -n1 /boot/config.txt)
+			if [ ! $lastline = "" ]; then # add empty line if not exists
+				echo "" >> /boot/config.txt
+			fi
 			echo "dtoverlay=w1-gpio,gpiopin=4,pullup=on" >> /boot/config.txt
 		else
 			echo "dtoverlay=w1-gpio,gpiopin=4,pullup=off" >> /boot/config.txt
