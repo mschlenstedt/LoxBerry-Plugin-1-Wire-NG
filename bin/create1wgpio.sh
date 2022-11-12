50e2763d61c3a7c8115acf88e19d1911d16bd0de
Michael Schlenstedt <Michael@schlenn.net>
2022-11-12 11:36:59 +0100

    Bugfixes for GPIO Master

--- bin/create1wgpio.sh
+++ bin/create1wgpio.sh
@@ -2,25 +2,20 @@
 
 pluginname=REPLACELBPPLUGINDIR
 
+# Clean config
+sed -i '/dtoverlay=w1-gpio,gpiopin=4.*$/d' /boot/config.txt
+
 gpio=$(jq -r '.gpio' $LBPCONFIG/$pluginname/owfs.json)
 
 if [ $gpio = "true" ]; then
-	grep -i "dtoverlay=w1-gpio,gpiopin=4" /boot/config.txt #überprüfen ob die enträge schon vorhanden sind
-	if [ $? -eq 0 ]; then 
-		echo"" #wenn sie schon vorhanden sind nichts tun
-	else 
-		#echo "" >> /boot/config.txt # Leerzeile
-		pullup=$(jq -r '.usb' $LBPCONFIG/$pluginname/owfs.json)
-		if [ $pullup = "true" ]; then 
-			lastline=$(tail -n1 /boot/config.txt)
-			if [ ! $lastline = "" ]; then # add empty line if not exists
-				echo "" >> /boot/config.txt
-			fi
-			echo "dtoverlay=w1-gpio,gpiopin=4,pullup=on" >> /boot/config.txt
-		else
-			echo "dtoverlay=w1-gpio,gpiopin=4,pullup=off" >> /boot/config.txt
-		fi
+	sed -i '${/^$/d;}' /boot/config.txt # Remove last blank line
+	echo "" >> /boot/config.txt
+	pullup=$(jq -r '.pullup' $LBPCONFIG/$pluginname/owfs.json)
+	echo "$pullup"
+	if [ $pullup = "true" ]; then 
+		echo "dtoverlay=w1-gpio,gpiopin=4,pullup=on" >> /boot/config.txt
+	else
+		echo "dtoverlay=w1-gpio,gpiopin=4,pullup=off" >> /boot/config.txt
 	fi
-else
-	sed -i 's/dtoverlay=w1-gpio,gpiopin=4.*$//g' /boot/config.txt
 fi
+exit 0
